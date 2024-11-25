8977fe76768abe13a85e033769d72b28bc2c1ea2
Aditya <adityamaurya0401@gmail.com>
2024-11-25 23:22:52 +0530

    third

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config()
-const bcrypt = require("bcrypt")
+const bcrypt = require('bcryptjs');
 const express=require("express")
 const cors=require("cors")
 const jwt=require("jsonwebtoken")
